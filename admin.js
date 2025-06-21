f33f479127351c5a031e8c679cdbbd2c54710f34
ezehmark <ezehmark@gmail.com>
2025-06-21 10:29:53 +0100

    new build

--- admin.js
+++ admin.js
@@ -1,7 +1,7 @@
 const { initializeApp, applicationDefault, cert } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
 
-const serviceAccount = process.env.firebaseAdmin;
+const serviceAccount = JSON.stringify(process.env.firebaseAdmin);
 initializeApp({
   credential: cert(serviceAccount)
 });
