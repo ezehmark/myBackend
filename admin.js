e534b2ba64e4ec62978e0fc27593607f61c32b2a
ezehmark <ezehmark@gmail.com>
2025-06-21 05:44:39 +0100

    new build

--- admin.js
+++ admin.js
@@ -1,7 +1,8 @@
 const { initializeApp, applicationDefault, cert } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
-const serviceAccount = require("./blocavax-firebase-adminsdk-fbsvc-24f722cc43.json");
+const path = require("path");
 
+const serviceAccount = require(path.resolve(__dirname, "blocavax-firebase-adminsdk-fbsvc-24f722cc43.json"));
 initializeApp({
   credential: cert(serviceAccount)
 });
